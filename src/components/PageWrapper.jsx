6adbca17776df80fbce371fa10c1ea342a80d353
MoccaGod <azminailalhadi28@gmail.com>
2025-06-17 21:03:48 +0700

    Add live chat

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
 import { motion } from "framer-motion";
-import WhatsappChat from "./WhatsappChat";
 
 const PageWrapper = ({ children }) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://embed.tawk.to/68516dfd75bff4190dddbd45/1itv1bacu";
+    script.async = true;
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+    document.body.appendChild(script);
+
+    // Optional cleanup
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -9,7 +23,6 @@ const PageWrapper = ({ children }) => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
     >
-      <WhatsappChat />
       {children}
     </motion.div>
   );
